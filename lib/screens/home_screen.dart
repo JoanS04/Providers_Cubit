8e70e5a2acb0267ab902bf8ebc7040b0a18d8a02
JoanS04 <joansanchez@paucasesnovescifp.cat>
2023-11-28 20:18:06 +0100

    Cards

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,39 +21,89 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
-      body: ListView(
-      padding: const EdgeInsets.all(10.0),
-      children: [
-         _card(),
-         SizedBox(height: 30.0),
-        ]),
+      body: Row(
+          children: [
+             _cardArticle(),
+             Expanded(child: SizedBox()),
+             _cardCarts()
+            ]),
       );
   }
-  Widget _card() {
-    return Card(
-      elevation: 10,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-      child: Column(
-        children: [
-          const ListTile(
-            leading: Icon(Icons.photo_album, color: Colors.blue),
-            title: Text('Títol de la targeta'),
-            subtitle: Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'),
-
-
+  Widget _cardArticle() {
+    final targeta = Column(
+      children: [
+        const FadeInImage(
+          placeholder: NetworkImage('https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg'), 
+          image: NetworkImage('https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg'),
+          fadeInDuration: Duration(milliseconds: 100),
+          width: 200,
+          fit: BoxFit.cover,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(onPressed: (){}, child: const Icon(Icons.remove)),
-              Expanded(child: SizedBox(),),
-              Text("Preu"),
-              Expanded(child: SizedBox(),),
-              TextButton(onPressed: (){}, child: const Icon(Icons.add)),
-            ],
-          )
-        ],
-      ),
+        Row(
+          children: [
+            ElevatedButton(onPressed: ()=>{}, child: const Icon(Icons.remove)),
+            const Expanded(child: SizedBox()),
+            const Text("preu"),
+            const Expanded(child: SizedBox()),
+            ElevatedButton(onPressed: ()=>{}, child: const Icon(Icons.add))
+          ],
+        ),
+      ],
     );
+    return Container(
+      width: 200,
+      height: 190,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 143, 143, 143).withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 10,
+            offset: Offset(0, 3),
+          ),
+        ],
+        // color: Colors.red
+    ),
+      child: ClipRRect(
+        child: targeta,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+ );
+ 
+  }
+  Widget _cardCarts() {
+    final targeta = Row(
+      children: [
+        const FadeInImage(
+          placeholder: NetworkImage('https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg'), 
+          image: NetworkImage('https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg'),
+          fadeInDuration: Duration(milliseconds: 100),
+          width: 50,
+          fit: BoxFit.cover,
+          ),
+          Expanded(child: Text("Cantidad preutotal", textAlign: TextAlign.center,)),
+      ],
+    );
+    return Container(
+      width: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 143, 143, 143).withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 10,
+            offset: Offset(0, 3),
+          ),
+        ],
+        // color: Colors.red
+    ),
+      child: ClipRRect(
+        child: targeta,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+ );
+ 
   }
 }
